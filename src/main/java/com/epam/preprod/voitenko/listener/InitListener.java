542e79b7d6beb76222c0493dfaec69fac59db07b
Yurii Voitenko <Yurii_Voitenko@epam.com>
2017-08-08 17:39:49 +0300

    Created listener for select strategy

---
+++ src/main/java/com/epam/preprod/voitenko/listener/InitListener.java
@@ -0,0 +1,44 @@
+package com.epam.preprod.voitenko.listener;
+
+import com.epam.preprod.voitenko.strategy.CaptchaStrategy;
+import com.epam.preprod.voitenko.strategy.CookieCaptchaStrategy;
+import com.epam.preprod.voitenko.strategy.InputHiddenCaptchaStorage;
+import com.epam.preprod.voitenko.strategy.SessionCaptchaStrategy;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InitListener implements ServletContextListener {
+    private Map<String, CaptchaStrategy> strategyMap;
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        initialize();
+        createStrategy(servletContextEvent);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        // NOP
+    }
+
+    private void createStrategy(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        String nameStrategy = servletContext.getInitParameter("CaptchaStrategy");
+        CaptchaStrategy captchaStrategy = new SessionCaptchaStrategy();         // default
+        if (strategyMap.containsKey(nameStrategy)) {
+            captchaStrategy = strategyMap.get(nameStrategy);
+        }
+        servletContext.setAttribute("strategy", captchaStrategy);
+    }
+
+    private void initialize() {
+        strategyMap = new HashMap<>();
+        strategyMap.put("session", new SessionCaptchaStrategy());
+        strategyMap.put("cookie", new CookieCaptchaStrategy());
+        strategyMap.put("hidden", new InputHiddenCaptchaStorage());
+    }
+}
