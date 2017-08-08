a37a73a6c3f9b07f1cbb314c32b17b9cd0740569
Yurii Voitenko <Yurii_Voitenko@epam.com>
2017-08-08 17:31:05 +0300

    Added RegisterBean and UserBean

---
+++ src/main/java/com/epam/preprod/voitenko/bean/RegisterBean.java
@@ -0,0 +1,61 @@
+package com.epam.preprod.voitenko.bean;
+
+public class RegisterBean {
+    private String fullName;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private String repeatedPassword;
+
+    public RegisterBean() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatedPassword() {
+        return repeatedPassword;
+    }
+
+    public void setRepeatedPassword(String repeatedPassword) {
+        this.repeatedPassword = repeatedPassword;
+    }
+}
