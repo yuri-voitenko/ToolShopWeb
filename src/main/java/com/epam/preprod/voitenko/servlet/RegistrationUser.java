815a660665ff177ab0a6751e969b2097097fac50
Yurii Voitenko <Yurii_Voitenko@epam.com>
2017-08-09 14:30:07 +0300

    Added pattern PRG for registration servlet

--- src/main/java/com/epam/preprod/voitenko/servlet/RegistrationUser.java
+++ src/main/java/com/epam/preprod/voitenko/servlet/RegistrationUser.java
@@ -12,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Map;
 
@@ -20,31 +21,43 @@ public class RegistrationUser extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
-
+        removeSessionAttribute(httpServletRequest, "regBean");
+        removeSessionAttribute(httpServletRequest, "errors");
+        removeSessionAttribute(httpServletRequest, "successRegistration");
+        httpServletRequest.getRequestDispatcher("/viewRegisterForm").forward(httpServletRequest, httpServletResponse);
     }
 
     @Override
     protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         RegisterBean regBean = Service.extractRegisterBean(httpServletRequest);
         Map<String, String> errors = ValidatorUtil.validate(regBean);
-
+        HttpSession session = httpServletRequest.getSession();
         validateCaptcha(httpServletRequest);
 
         if (!errors.isEmpty()) {
-            httpServletRequest.setAttribute("regBean", regBean);
+            session.setAttribute("regBean", regBean);
         } else {
             UserBean user = Service.fillUserBean(regBean);
             if (UserRepository.containsUser(user)) {
                 regBean.setEmail("");
-                httpServletRequest.setAttribute("regBean", regBean);
+                session.setAttribute("regBean", regBean);
                 errors.put("Email", "Registration fail!<br>User with such login(email) already exists!");
             } else {
                 UserRepository.addUser(user);
-                httpServletRequest.setAttribute("successRegistration", "Registration completed successfully!");
+                session.setAttribute("successRegistration", "Registration completed successfully!");
             }
         }
-        httpServletRequest.setAttribute("errors", errors);
-        getServletContext().getRequestDispatcher("/viewRegisterForm").forward(httpServletRequest, httpServletResponse);
+        session.setAttribute("errors", errors);
+        httpServletResponse.sendRedirect("/registerUser");
+    }
+
+    private void removeSessionAttribute(HttpServletRequest httpServletRequest, String key) {
+        HttpSession session = httpServletRequest.getSession();
+        Object object = session.getAttribute(key);
+        if (object != null) {
+            httpServletRequest.setAttribute(key, object);
+            session.removeAttribute(key);
+        }
     }
 
     private void validateCaptcha(HttpServletRequest httpServletRequest) {
