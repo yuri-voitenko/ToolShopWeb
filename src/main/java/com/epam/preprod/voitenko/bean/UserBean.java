84a6e8eb0191aea20bb1dfd65123f59b468dd4b0
Yurii Voitenko <Yurii_Voitenko@epam.com>
2017-08-11 17:40:47 +0300

    Modified UserBean with according to the database table

--- src/main/java/com/epam/preprod/voitenko/bean/UserBean.java
+++ src/main/java/com/epam/preprod/voitenko/bean/UserBean.java
@@ -3,45 +3,30 @@ package com.epam.preprod.voitenko.bean;
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class UserBean {
-    private String fullName;
-    private String address;
-    private String phoneNumber;
+    private int id;
     private String email;
     private String password;
+    private String fullName;
+    private String phoneNumber;
+    private String address;
 
     public UserBean() {
     }
 
-    public UserBean(String fullName, String address, String phoneNumber, String email, String password) {
-        this.fullName = fullName;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
+    public UserBean(String email, String password, String fullName, String phoneNumber, String address) {
         this.email = email;
         this.password = DigestUtils.md5Hex(password + fullName);
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
         this.fullName = fullName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
+        this.phoneNumber = phoneNumber;
         this.address = address;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public int getId() {
+        return id;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getEmail() {
@@ -57,9 +42,32 @@ public class UserBean {
     }
 
     public void setPassword(String password) {
-        this.password = DigestUtils.md5Hex(password + fullName);
+        this.password = password;
     }
 
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -69,29 +77,35 @@ public class UserBean {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
+
         UserBean userBean = (UserBean) o;
-        if (fullName != null ? !fullName.equals(userBean.fullName) : userBean.fullName != null) {
-            return false;
-        }
-        if (address != null ? !address.equals(userBean.address) : userBean.address != null) {
-            return false;
-        }
-        if (phoneNumber != null ? !phoneNumber.equals(userBean.phoneNumber) : userBean.phoneNumber != null) {
+
+        if (id != userBean.id) {
             return false;
         }
         if (email != null ? !email.equals(userBean.email) : userBean.email != null) {
             return false;
         }
-        return password != null ? password.equals(userBean.password) : userBean.password == null;
+        if (password != null ? !password.equals(userBean.password) : userBean.password != null) {
+            return false;
+        }
+        if (fullName != null ? !fullName.equals(userBean.fullName) : userBean.fullName != null) {
+            return false;
+        }
+        if (phoneNumber != null ? !phoneNumber.equals(userBean.phoneNumber) : userBean.phoneNumber != null) {
+            return false;
+        }
+        return address != null ? address.equals(userBean.address) : userBean.address == null;
     }
 
     @Override
     public int hashCode() {
-        int result = fullName != null ? fullName.hashCode() : 0;
-        result = 31 * result + (address != null ? address.hashCode() : 0);
-        result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
+        int result = id;
         result = 31 * result + (email != null ? email.hashCode() : 0);
         result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (fullName != null ? fullName.hashCode() : 0);
+        result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
         return result;
     }
 }
