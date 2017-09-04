996c2844534e5254d92e40d68bf474a459e6411d
Yurii Voitenko <Yurii_Voitenko@epam.com>
2017-09-04 14:02:15 +0300

    pulled changes from master

--- src/main/java/com/epam/preprod/voitenko/servlet/cart/AddToolToCart.java
+++ src/main/java/com/epam/preprod/voitenko/servlet/cart/AddToolToCart.java
@@ -2,10 +2,7 @@ package com.epam.preprod.voitenko.servlet.cart;
 
 import com.epam.preprod.voitenko.entity.Cart;
 import com.epam.preprod.voitenko.entity.ElectricToolEntity;
-import com.epam.preprod.voitenko.handler.DataSourceHandler;
-import com.epam.preprod.voitenko.service.ToolService;
 import com.epam.preprod.voitenko.util.ServiceUtil;
-import org.json.simple.JSONObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,16 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.sql.DataSource;
 import java.io.IOException;
-import java.io.Writer;
 import java.math.BigDecimal;
 
 import static com.epam.preprod.voitenko.constant.Constatns.Keys.CART;
-import static com.epam.preprod.voitenko.constant.Constatns.Keys.CART_QUANTITY;
-import static com.epam.preprod.voitenko.constant.Constatns.Keys.CART_TOTAL;
-import static com.epam.preprod.voitenko.constant.Constatns.Keys.ID;
-import static com.epam.preprod.voitenko.constant.Constatns.Keys.TOTAL_COST_SPECIFIC_TOOL;
 
 @WebServlet("/addToolToCart")
 public class AddToolToCart extends HttpServlet {
@@ -31,16 +22,7 @@ public class AddToolToCart extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
         Cart<ElectricToolEntity> cart = ServiceUtil.getCart(session);
-
-        String strToolID = req.getParameter(ID);
-        Integer toolID = 0;
-        if (strToolID != null) {
-            toolID = Integer.parseInt(strToolID);
-        }
-
-        DataSource dataSource = DataSourceHandler.getInstance().getDataSource();
-        ToolService toolService = new ToolService(dataSource);
-        ElectricToolEntity tool = toolService.getToolById(toolID);
+        ElectricToolEntity tool = ServiceUtil.extractElectricToolEntity(req);
 
         BigDecimal totalCostSpecificTool = tool.getCost();
         BigDecimal quantitySpecificTool = new BigDecimal(cart.addProduct(tool));
@@ -48,12 +30,6 @@ public class AddToolToCart extends HttpServlet {
         totalCostSpecificTool = totalCostSpecificTool.setScale(2, BigDecimal.ROUND_HALF_UP);
 
         session.setAttribute(CART, cart);
-
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put(CART_TOTAL, cart.getTotalSumPurchase().toString());
-        jsonObject.put(CART_QUANTITY, cart.getTotalQuantityProducts().toString());
-        jsonObject.put(TOTAL_COST_SPECIFIC_TOOL, totalCostSpecificTool.toString());
-        Writer writer = resp.getWriter();
-        jsonObject.writeJSONString(writer);
+        ServiceUtil.writeJSONObject(resp, cart.getTotalSumPurchase(), cart.getTotalQuantityProducts(), totalCostSpecificTool);
     }
 }
